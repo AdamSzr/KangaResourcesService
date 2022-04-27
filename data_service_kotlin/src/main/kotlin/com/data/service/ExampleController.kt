ae90f96dd038a4e433b8f8ef5a32bdd18b82e4e5
AdamSzr <adam.szr98@gmail.com>
2022-04-27 17:44:59 +0200

    Endpoints are working

---
+++ data_service_kotlin/src/main/kotlin/com/data/service/ExampleController.kt
@@ -0,0 +1,21 @@
+package com.data.service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ExampleController {
+    @GetMapping("/example")
+    public fun two(@RequestParam path:String): String {
+        System.out.println(path)
+        return "-> $path";
+    }
+
+//    @GetMapping("/example")
+//    public fun one(): String {
+//        return "-> XD";
+//    }
+}
