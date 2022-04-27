d136e873ca60e0014db1cc626409f002a03e5b18
AdamSzr <adam.szr98@gmail.com>
2022-04-27 14:10:03 +0200

    create empty spring kotlin app

---
+++ data_service_kotlin/src/main/kotlin/com/data/service/DataServiceKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.data.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataServiceKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataServiceKotlinApplication>(*args)
+}
