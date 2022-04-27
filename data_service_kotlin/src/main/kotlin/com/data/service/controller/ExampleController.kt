8b4a75349d14e349f80785fa2b378e9a09ba1cb4
AdamSzr <adam.szr98@gmail.com>
2022-04-27 19:18:46 +0200

    mode

--- data_service_kotlin/src/main/kotlin/com/data/service/controller/ExampleController.kt
+++ data_service_kotlin/src/main/kotlin/com/data/service/controller/ExampleController.kt
@@ -1,4 +1,4 @@
-package com.data.service.controller
+package com.data.service
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
--- data_service_kotlin/src/main/kotlin/com/data/service/model/ErrorStructure.java
+++ data_service_kotlin/src/main/kotlin/com/data/service/model/ErrorStructure.java
@@ -1,4 +1,35 @@
-package com.data.service.model.ErrorStructure;
+package com.data.service.model;
 
-public class ts {
+public class ErrorStructure {
+    private int code;
+    private String description;
+    private ErrorStructure(){
+    }
+
+    void setCode(int code) {
+        this.code = code;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    int getCode() {
+        return code;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    public static ErrorStructure code(int number){
+      ErrorStructure z = new ErrorStructure();
+      z.code=number;
+      return z;
+    }
+
+    public ErrorStructure description(String description){
+        setDescription(description);
+        return this;
+    }
 }
