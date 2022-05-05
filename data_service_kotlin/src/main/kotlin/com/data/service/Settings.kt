4e3391667038be8ce525ba66c08cc91453815f4d
AdamSzr <adam.szr98@gmail.com>
2022-05-05 23:46:49 +0200

    Prepare to run on new PC disk

--- data_service_kotlin/src/main/kotlin/com/data/service/Settings.kt
+++ data_service_kotlin/src/main/kotlin/com/data/service/Settings.kt
@@ -1,5 +1,6 @@
 package com.data.service
 
 
-val PATH_PUBLIC_DIR = System.getenv("PWD") + "static/data/";
+//var PATH_PUBLIC_DIR: String = System.getenv("DATA_ROOT_DIR");
+var PATH_PUBLIC_DIR: String = "C:\\Users\\adam_pc\\Documents\\Coding\\Work\\KangaResourcesService\\data_service_kotlin\\src\\main\\resources\\static\\data";
 const val FILE_DOWNLOAD_ENABLE = false
